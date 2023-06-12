246eb6e328c80139537e0f57de33fe8d6168192d
ApollineLH <apolline.le@gmail.com>
2023-06-12 12:22:21 +0200

    add swagger

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require('dotenv');
 
-require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const helloRoutes = require('./routes/helloRoutes');
-const authRoutes = require ('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
 const authMiddleware = require('./middlewares/authMiddleware');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 app.use(bodyParser.json());
 
@@ -16,15 +18,31 @@ app.use(authMiddleware.verifyToken);
 app.use('/api', helloRoutes);
 app.use('/api/auth', authRoutes);
 
-
 // Gestion des erreurs
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Erreur interne du serveur' });
+  console.error(err.stack);
+  res.status(500).json({ message: 'Erreur interne du serveur' });
 });
+
+// Configuration Swagger
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'Documentation de l\'API'
+    }
+  },
+  apis: ['./routes/*.js'] // Chemin vers les fichiers contenant les routes à documenter
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+// Route pour la documentation Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Démarrage du serveur
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Serveur démarré sur le port ${port}`);
+  console.log(`Serveur démarré sur le port ${port}`);
 });
